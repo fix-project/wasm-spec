45fb4b3966d0e387d8bb25203b214d1454b9ad2b
keithw <keithw@users.noreply.github.com>
2022-10-05 09:00:58 +0000

    deploy: 58cd4c33f093038d2f9395ee976f716fc0e365c1

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0 (Draft 2022-06-09)',
-    LANGUAGE: 'None',
+    VERSION: '2.0 (Draft 2022-10-05)',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
